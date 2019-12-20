66a9373bb4dbfa5085a28459acb43793df1fba06
Daniel Royo <danislash@gmail.com>
2019-12-20 20:16:03 +0100

    refactoring rover process

--- src/test/java/com/example/Rover.java
+++ src/test/java/com/example/Rover.java
@@ -1,6 +1,7 @@
 package com.example;
 
 import java.util.Objects;
+import java.util.stream.Stream;
 
 public class Rover {
     private final int x;
@@ -39,15 +40,22 @@ public class Rover {
 
 
     //regla de los 3 strikes
-    public Rover accept(String commands) {
-        if (commands.equals("r")) {
-            return new Rover(0, 0, this.position.turnRight());
-        } else if (commands.equals("l")) {
-            return new Rover(0, 0, this.position.turnLeft());
-        } else if (commands.equals("b")) {
-            return new Rover(x, y - 1, position);
-        } else
-            return new Rover(x, y + 1, position);
+    public Rover accept(String rawCommandsRepresentation) {
+
+        String[] commands = rawCommandsRepresentation.split("");
+
+        //Stream.of(commands).re
+
+        for (String command : commands) {
+            if (command.equals("r")) {
+                return new Rover(0, 0, this.position.turnRight());
+            } else if (command.equals("l")) {
+                return new Rover(0, 0, this.position.turnLeft());
+            } else if (command.equals("b")) {
+                return new Rover(x, y - 1, position);
+            } else
+                return new Rover(x, y + 1, position);
+        }
     }
 
 }
